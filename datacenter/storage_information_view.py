89dbf7598931f7fa03611f7191c1baf24b1720b7
Katsutami7moto <nclsf87@gmail.com>
2022-06-15 17:25:18 +0300

    Show users visiting the vault

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,36 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from datetime import datetime, timezone, timedelta
+from django.utils.timezone import localtime
+from django.db import models
+
+
+def get_duration(visit_local_time: datetime) -> timedelta:
+    return datetime.now(timezone.utc) - visit_local_time
+
+
+def format_duration(delta: timedelta) -> str:
+    delta_total_seconds = int(delta.total_seconds())
+    hours, remainder = divmod(delta_total_seconds, 60 * 60)
+    minutes, seconds = divmod(remainder, 60)
+    return f'{hours:02d}:{minutes:02d}:{seconds:02d}'
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    non_closed_visits = []
+    visits_not_leaved = Visit.objects.filter(leaved_at=None)
+    for visit in visits_not_leaved:
+        visiter_name = visit.passcard.owner_name
+        visit_local_time = visit.entered_at
+        duration = format_duration(get_duration(localtime(visit_local_time)))
+        visit_info = {
+            'who_entered': visiter_name,
+            'entered_at': visit_local_time,
+            'duration': duration,
         }
-    ]
+        non_closed_visits.append(visit_info)
+
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
